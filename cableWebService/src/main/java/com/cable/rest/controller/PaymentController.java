8c0eacf97f85b89ff00bb5206be7f6022d508fc8
bharathmit <bharathkumar.feb14@gmail.com>
2016-06-23 15:51:42 +0530

    cableWebService payment service added

--- cableWebService/src/main/java/com/cable/rest/controller/PaymentController.java
+++ cableWebService/src/main/java/com/cable/rest/controller/PaymentController.java
@@ -1,5 +1,34 @@
 package com.cable.rest.controller;
 
-public class PaymentController {
+import lombok.extern.log4j.Log4j;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cable.rest.dto.PaymentDetailDto;
+import com.cable.rest.response.ResponseResource;
+import com.cable.rest.search.PaymentSearch;
+
+@RestController
+@RequestMapping("/payment")
+@Log4j
+public class PaymentController extends BaseController {
+	
+	
+	@RequestMapping(value="/savepayment",method=RequestMethod.POST)
+	public Object savePayment(@RequestBody PaymentDetailDto reqObject){
+		return  sendtoMQ(reqObject, "savePayment", "paymentService");
+	}
+	
+	@RequestMapping(value="/paymentlist",method=RequestMethod.POST)
+	public Object getPaymentList(@RequestBody PaymentSearch searchObject){
+		return sendtoMQ(searchObject, "getPaymentList", "projectService");
+	}
+	
+	@RequestMapping(value="/deletepayment",method=RequestMethod.POST)
+	public ResponseResource deletePayment(@RequestBody PaymentSearch searchObject){
+		return (ResponseResource) sendtoMQ(searchObject, "deletePayment", "projectService");
+	}
 }
--- cableWebService/src/main/java/com/cable/rest/service/PaymentService.java
+++ cableWebService/src/main/java/com/cable/rest/service/PaymentService.java
@@ -1,19 +1,28 @@
 package com.cable.rest.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import lombok.extern.log4j.Log4j;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cable.rest.dto.PaymentDetailDto;
 import com.cable.rest.exception.RestException;
 import com.cable.rest.model.PaymentDetails;
-import com.cable.rest.model.ZipCode;
 import com.cable.rest.repository.PaymentDetailJPARepo;
 import com.cable.rest.response.ErrorCodeDescription;
+import com.cable.rest.response.ResponseResource;
+import com.cable.rest.search.PaymentSearch;
 import com.cable.rest.utils.ModelEntityMapper;
 
 @Service
@@ -27,6 +36,7 @@ public class PaymentService {
 	@Autowired
 	PaymentDetailJPARepo paymentDetailJPARepo;
 	
+	@Transactional
 	public PaymentDetailDto savePayment(PaymentDetailDto Object){
 		try{
 			
@@ -38,6 +48,52 @@ public class PaymentService {
 			throw new RestException(ErrorCodeDescription.TRANSACTION_FAILED);
 		}
 		return Object;
+	}
+	
+	@Transactional
+	@Rollback(false)
+	public List<PaymentDetailDto> getPaymentList(PaymentSearch search) {
+		try{
+			
+			List<PaymentDetailDto> paymentList=new ArrayList<PaymentDetailDto>();
+			Session session = entityManager.unwrap(Session.class);
+			
+			Criteria criteria= session.createCriteria(PaymentDetails.class);
+			
+			/*if(search.getOrgId() !=null && search.getOrgId() >0l){
+				criteria.add(Restrictions.eq("orgId", search.getOrgId()));
+			}
+			
+			if(!StringUtils.isEmpty(search.getOrgName())){
+				criteria.add(Restrictions.eq("orgName", search.getOrgName()));
+			}
+			
+			if(!StringUtils.isEmpty(search.getPinCode())){
+				criteria.add(Restrictions.eq("pinCode", search.getPinCode()));
+			}*/
+			List<PaymentDetails> list=criteria.list();
+			
+			paymentList=criteria.setResultTransformer(Transformers.aliasToBean(PaymentDetailDto.class)).list();
+			
+			return paymentList;
+			
+		}
+		catch(Exception e){
+			throw new RestException(ErrorCodeDescription.DATA_ACCESS);
+		}
+		
+	}
+	
+	@Transactional
+	public ResponseResource deletePayment(PaymentSearch search) {
+		try{
+			//paymentDetailJPARepo.delete(search.getOrgId());
+			return new ResponseResource(ErrorCodeDescription.TRANSACTION_SUCCESS);
+		}
+		catch(Exception e){
+			throw new RestException(ErrorCodeDescription.DATA_ACCESS);
+		}
+		
 	}
 	
 }
