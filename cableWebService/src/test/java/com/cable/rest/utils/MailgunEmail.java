5c4322d60b86d0e5cfb885fccbd88798a79e2b63
bharath <bharathkumar.feb14@gmail.com>
2016-08-11 21:31:40 +0530

    mail test

--- cableWebService/src/test/java/com/cable/rest/utils/MailgunEmail.java
+++ cableWebService/src/test/java/com/cable/rest/utils/MailgunEmail.java
@@ -6,13 +6,13 @@ import java.util.Properties;
 import java.util.Date;
 
 import javax.mail.*;
-
 import javax.mail.internet.*;
 
 import com.sun.mail.smtp.*;
 
 
 public class MailgunEmail {
+	
     public static void main(String args[]) throws Exception {
         Properties props = System.getProperties();
         props.put("mail.smtps.host","smtp.mailgun.org");
@@ -21,15 +21,21 @@ public class MailgunEmail {
         Message msg = new MimeMessage(session);
         msg.setFrom(new InternetAddress("bharathkumar.feb14@gmail.com"));
         msg.setRecipients(Message.RecipientType.TO,
-        InternetAddress.parse("bar@example.com", false));
+        InternetAddress.parse("bharathkumar.feb14@gmail.com", false));
         msg.setSubject("Hello");
         msg.setText("Testing some Mailgun awesomness");
         msg.setSentDate(new Date());
         SMTPTransport t =
             (SMTPTransport)session.getTransport("smtps");
-        t.connect("smtp.mailgun.com", "bharathkumar.feb14@gmail.com", "bharath214");
+        t.connect("smtp.mailgun.com", "postmaster@sandbox8d5e29efe3614d11b260af8dc32e76b4.mailgun.org", "2b99f1f7cf45b987142ce3c95d93e82c");
         t.sendMessage(msg, msg.getAllRecipients());
         System.out.println("Response: " + t.getLastServerResponse());
         t.close();
     }
+    
+    
+    
+    
+    
+    
 }
